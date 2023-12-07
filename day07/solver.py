456e37b27f370b41034e11e1b6c6d6f0a20980af
Nikolai Hartmann <nikoladze@posteo.de>
2023-12-07 09:51:28 +0100

    cleanup

--- day07/solver.py
+++ day07/solver.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
+import sys
 from collections import Counter
 from itertools import product
-import sys
 from pathlib import Path
 
 from aoc import utils
-
 
 measure_time = utils.stopwatch()
 
@@ -15,52 +14,58 @@ measure_time = utils.stopwatch()
 def parse(raw_data):
     return [line.split() for line in raw_data.strip().splitlines()]
 
-# PART 1
-@measure_time
-def solve1(data):
-    STRENGTHS = {k: i for i, k in enumerate("AKQJT98765432")}
-    cards = []
-    for hand, bid in data:
-        counts = Counter(hand)
-        if any(count == 5 for count in counts.values()):
-            t = 0
-        elif any(count == 4 for count in counts.values()):
-            t = 1
-        elif any(count == 3 for count in counts.values()) and any(count == 2 for count in counts.values()):
-            t = 2
-        elif any(count == 3 for count in counts.values()):
-            t = 3
-        elif sum(count == 2 for count in counts.values()) == 2:
-            t = 4
-        elif any(count == 2 for count in counts.values()):
-            t = 5
-        else:
-            t = 6
-        cards.append({"hand": hand, "counts": counts, "type": t, "bid": int(bid), "strengths": tuple(STRENGTHS[k] for k in hand)})
-    cards.sort(key=lambda card: (card["type"], card["strengths"]), reverse=True)
-    return sum(rank * card["bid"] for rank, card in enumerate(cards, start=1))
+
+def any_n(counts, n):
+    return any(count == n for count in counts.values())
 
 
 def get_type(counts):
-    if any(count == 5 for count in counts.values()):
+    if any_n(counts, 5):
+        # Five of a kind
         return 0
-    if any(count == 4 for count in counts.values()):
+    if any_n(counts, 4):
+        # Four of a kind
         return 1
-    if any(count == 3 for count in counts.values()) and any(count == 2 for count in counts.values()):
+    if any_n(counts, 3) and any_n(counts, 2):
+        # Full House
         return 2
-    if any(count == 3 for count in counts.values()):
+    if any_n(counts, 3):
+        # Three of a kind
         return 3
     if sum(count == 2 for count in counts.values()) == 2:
+        # Two pair
         return 4
-    if any(count == 2 for count in counts.values()):
+    if any_n(counts, 2):
+        # One pair
         return 5
+    # High card
     return 6
+
+
+# PART 1
+@measure_time
+def solve1(data):
+    strengths = {k: i for i, k in enumerate("AKQJT98765432")}
+    cards = []
+    for hand, bid in data:
+        counts = Counter(hand)
+        cards.append(
+            {
+                "hand": hand,
+                "counts": counts,
+                "type": get_type(counts),
+                "bid": int(bid),
+                "strengths": tuple(strengths[k] for k in hand),
+            }
+        )
+    cards.sort(key=lambda card: (card["type"], card["strengths"]), reverse=True)
+    return sum(rank * card["bid"] for rank, card in enumerate(cards, start=1))
 
 
 # PART 2
 @measure_time
 def solve2(data):
-    STRENGTHS = {k: i for i, k in enumerate("AKQT98765432J")}
+    strengths = {k: i for i, k in enumerate("AKQT98765432J")}
     non_jokers = "AKQT98765432"
     cards = []
     for hand, bid in data:
@@ -75,10 +80,16 @@ def solve2(data):
             new_t = get_type(counts)
             if new_t < t:
                 t = new_t
-        cards.append({"hand": hand, "counts": counts, "type": t, "bid": int(bid), "strengths": tuple(STRENGTHS[k] for k in hand)})
+        cards.append(
+            {
+                "hand": hand,
+                "counts": counts,
+                "type": t,
+                "bid": int(bid),
+                "strengths": tuple(strengths[k] for k in hand),
+            }
+        )
     cards.sort(key=lambda card: (card["type"], card["strengths"]), reverse=True)
-    from pprint import pprint
-    pprint(cards)
     return sum(rank * card["bid"] for rank, card in enumerate(cards, start=1))
 
 
@@ -92,4 +103,3 @@ if __name__ == "__main__":
         print(f"{func:8}{time}s")
     print("----------------")
     print("total   {}s".format(sum(t for _, t in measure_time.times)))
-
