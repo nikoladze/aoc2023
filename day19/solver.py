1dd964bf0576a6711cb4fc83c6772e263799ef1d
Nikolai Hartmann <nikoladze@posteo.de>
2023-12-19 20:23:04 +0100

    add solution for day19

---
+++ day19/solver.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+import sys
+
+from aoc import utils
+
+measure_time = utils.stopwatch()
+
+
+@measure_time
+def parse(raw_data):
+    workflows_raw, ratings = raw_data.strip().split("\n\n")
+    workflows = {}
+    for workflow in workflows_raw.splitlines():
+        name, instructions = workflow.split("{")
+        instructions = instructions[:-1].split(",")
+        instructions = [ins.split(":") for ins in instructions]
+        workflows[name] = instructions
+    ratings = [dict(fields.split("=") for fields in rating[1:-1].split(",")) for rating in ratings.splitlines()]
+    ratings = [{k: int(v) for k, v in rating.items()} for rating in ratings]
+    return workflows, ratings
+
+
+def run_workflow(workflows, name, part):
+    workflow = workflows[name]
+    for ins in workflow:
+        match ins:
+            case (expr, res):
+                if not eval(expr, {}, dict(part)):
+                    continue
+            case (res,):
+                pass
+        match res:
+            case "A":
+                return True
+            case "R":
+                return False
+            case other:
+                return run_workflow(workflows, other, part)
+    return False
+
+
+# PART 1
+@measure_time
+def solve1(data):
+    workflows, ratings = data
+    solution = 0
+    for part in ratings:
+        if run_workflow(workflows, "in", part):
+            solution += sum(part.values())
+    return solution
+
+
+def find_bounds(workflows, name, bounds, leaves, indent=""):
+    workflow = workflows[name]
+    print()
+    print(indent, f"{name=}, {workflow=}")
+    for ins in workflow:
+        bounds = {k: list(v) for k, v in bounds.items()}
+        bounds_else = bounds
+        match ins:
+            case (expr, res):
+                key = expr[0]
+                op = expr[1]
+                val = int(expr[2:])
+                lower, upper = bounds[key]
+                lower_else, upper_else = lower, upper
+                print(indent, f"{lower=}, {upper=}")
+                if op == "<":
+                    # (lower, upper) < val
+                    if lower >= val:
+                        continue
+                    upper = val - 1
+                    lower_else = val
+                elif op == ">":
+                    # (lower, upper) > val
+                    if val >= upper:
+                        continue
+                    lower = val + 1
+                    upper_else = val
+                else:
+                    assert False
+                bounds[key] = (lower, upper)
+                print(indent, f"after {expr=}: {lower=}, {upper=}, {bounds=}")
+                bounds_else = {k: list(v) for k, v in bounds.items()}
+                bounds_else[key] = (lower_else, upper_else)
+            case (res,):
+                pass
+        match res:
+            case "A":
+                print(indent, f"append {bounds=}")
+                leaves.append(bounds)
+            case "R":
+                pass
+            case other:
+                find_bounds(workflows, other, bounds, leaves=leaves, indent=indent+"    ")
+        bounds = bounds_else
+
+
+# PART 2
+@measure_time
+def solve2(data):
+    workflows, _ = data
+    bounds = {key: (1, 4000) for key in "xmas"}
+    solution = 0
+    leaves = []
+    bounds = find_bounds(workflows, "in", bounds, leaves=leaves)
+    for l in leaves:
+        sub = 1
+        print(l)
+        for lower, upper in l.values():
+            sub *= (upper - lower) + 1 #?
+        solution += sub
+    return solution
+
+
+if __name__ == "__main__":
+    data = parse(open(Path(__file__).parent / "input.txt").read())
+    print("Part 1: {}".format(solve1(data)))
+    print("Part 2: {}".format(solve2(data)))
+
+    print("\nTime taken:")
+    for func, time in measure_time.times:
+        print(f"{func:8}{time}s")
+    print("----------------")
+    print("total   {}s".format(sum(t for _, t in measure_time.times)))
+
---
+++ day19/test_solver.py
@@ -0,0 +1,43 @@
+import pytest
+from solver import parse, solve1, solve2
+
+TESTDATA = """
+px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}
+"""
+
+@pytest.fixture
+def parsed_data():
+    return parse(TESTDATA)
+
+
+def test_parse():
+    data = parse(TESTDATA)
+    # asserts go here
+
+
+# PART 1
+def test_solve1(parsed_data):
+    solution = solve1(parsed_data)
+    assert solution == 19114
+
+
+# PART 2
+def test_solve2(parsed_data):
+    solution = solve2(parsed_data)
+    assert solution == 167409079868000
