7a29fd32ff621db697a3ba52e2237b33405fb42a
Nikolai Hartmann <nikoladze@posteo.de>
2023-12-20 13:08:29 +0100

    add solution for day17

---
+++ day17/test_solver.py
@@ -0,0 +1,39 @@
+import pytest
+from solver import parse, solve1, solve2
+
+TESTDATA = """
+2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533
+"""
+
+@pytest.fixture
+def parsed_data():
+    return parse(TESTDATA)
+
+
+def test_parse():
+    data = parse(TESTDATA)
+    # asserts go here
+
+
+# PART 1
+def test_solve1(parsed_data):
+    solution = solve1(parsed_data)
+    assert solution == 102
+
+
+# PART 2
+def test_solve2(parsed_data):
+    solution = solve2(parsed_data)
+    assert solution == 94
