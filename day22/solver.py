690dc2e7b2c3bb0fd25252ce92989c5d2d0ac4c3
Nikolai Hartmann <nikoladze@posteo.de>
2023-12-22 15:48:31 +0100

    cleanup

--- day22/solver.py
+++ day22/solver.py
@@ -20,29 +20,25 @@ def parse(raw_data):
     return out
 
 
-# PART 1
-@measure_time
-def solve1(data):
-    total = 0
+def let_fall(data):
     brick_for_coord = {}
     coords_for_brick = defaultdict(list)
     for i, ((x1, y1, z1), (x2, y2, z2)) in enumerate(data):
-    #for i, ((x1, y1, z1), (x2, y2, z2)) in zip("ABCDEFGHIJKLMNOP", data):
         for coord in product(range(x1, x2 + 1), range(y1, y2 + 1), range(z1, z2 + 1)):
             brick_for_coord[coord] = i
             coords_for_brick[i].append(coord)
 
-    # let fall
     while True:
         any_move = False
         for i, coords in sorted(
-            list(map(list, coords_for_brick.items())), key=lambda x: sum(z for _, _, z in x[1])
+            list(map(list, coords_for_brick.items())),
+            key=lambda x: sum(z for _, _, z in x[1]),
         ):
             for x, y, z in coords:
                 if z == 1:
                     # reached bottom
                     break
-                if brick_for_coord.get((x, y, z-1), i) != i:
+                if brick_for_coord.get((x, y, z - 1), i) != i:
                     # hit other brick
                     break
             else:
@@ -55,97 +51,54 @@ def solve1(data):
                     brick_for_coord[coord] = i
                     coords_for_brick[i].append(coord)
         if not any_move:
-            break
+            return brick_for_coord, coords_for_brick
 
+
+def get_supporting_structure(brick_for_coord, coords_for_brick):
     supporting = defaultdict(set)
     supported_by = defaultdict(set)
     for i, coords in coords_for_brick.items():
         for x, y, z in coords:
-            if (j := brick_for_coord.get((x, y, z+1), i)) != i:
+            if (j := brick_for_coord.get((x, y, z + 1), i)) != i:
                 supporting[i].add(j)
                 supported_by[j].add(i)
+    return supporting, supported_by
 
-    # print(supporting)
-    # print(supported_by)
 
-    solution = 0
-    for i in coords_for_brick:
-        if all(len(supported_by[j]) > 1 for j in supporting[i]):
-            print(i)
-            solution += 1
-
-    return solution
+# PART 1
+@measure_time
+def solve1(data):
+    brick_for_coord, coords_for_brick = let_fall(data)
+    supporting, supported_by = get_supporting_structure(
+        brick_for_coord, coords_for_brick
+    )
+    return sum(
+        all(len(supported_by[j]) > 1 for j in supporting[i]) for i in coords_for_brick
+    )
 
 
 # PART 2
 @measure_time
 def solve2(data):
-    total = 0
-    brick_for_coord = {}
-    coords_for_brick = defaultdict(list)
-    for i, ((x1, y1, z1), (x2, y2, z2)) in enumerate(data):
-    #for i, ((x1, y1, z1), (x2, y2, z2)) in zip("ABCDEFGHIJKLMNOP", data):
-        for coord in product(range(x1, x2 + 1), range(y1, y2 + 1), range(z1, z2 + 1)):
-            brick_for_coord[coord] = i
-            coords_for_brick[i].append(coord)
-
-    # let fall
-    while True:
-        any_move = False
-        for i, coords in sorted(
-            list(map(list, coords_for_brick.items())), key=lambda x: sum(z for _, _, z in x[1])
-        ):
-            for x, y, z in coords:
-                if z == 1:
-                    # reached bottom
-                    break
-                if brick_for_coord.get((x, y, z-1), i) != i:
-                    # hit other brick
-                    break
-            else:
-                coords = list(coords)
-                any_move = True
-                coords_for_brick[i].clear()
-                for x, y, z in coords:
-                    del brick_for_coord[x, y, z]
-                    coord = x, y, z - 1
-                    brick_for_coord[coord] = i
-                    coords_for_brick[i].append(coord)
-        if not any_move:
-            break
-
-    supporting = defaultdict(set)
-    supported_by = defaultdict(set)
-    for i, coords in coords_for_brick.items():
-        for x, y, z in coords:
-            if (j := brick_for_coord.get((x, y, z+1), i)) != i:
-                supporting[i].add(j)
-                supported_by[j].add(i)
-
-    # print(supporting)
-    # print(supported_by)
+    supporting, supported_by = get_supporting_structure(*let_fall(data))
 
     def count_supporting(i):
         total = 0
         disintegrated = set()
+
         def count(i):
             nonlocal total
-            print(f"checking {i=}")
             disintegrated.add(i)
             for j in supporting[i]:
-                print(f"{j=}, {supported_by[j]=}")
                 if any(ji not in disintegrated for ji in supported_by[j]):
                     continue
-                print("increasing!", total)
                 total += 1
                 count(j)
+
         count(i)
-        print(f"{i=}, {total=}")
         return total
 
-
     return sum(count_supporting(i) for i in list(supporting.keys()))
-
 
 
 if __name__ == "__main__":
