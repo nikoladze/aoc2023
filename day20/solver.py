c6ffeafaaa79e9feeb9edda5c100d37fc3bf1f56
Nikolai Hartmann <nikoladze@posteo.de>
2023-12-30 18:54:55 +0100

    add solution for day20, part2

--- day20/solver.py
+++ day20/solver.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 from collections import deque
+from itertools import count
 from pathlib import Path
 import sys
+import math
 
 from aoc import utils
 
@@ -107,6 +109,31 @@ def solve1(data):
     return nlow * nhigh
 
 
+def get_first_low_and_cycle(modules, name):
+    c = count()
+    first = None
+    prev = None
+    while True:
+        i = next(c)
+        q = deque([("button", "broadcaster", False)])
+        while q:
+            src, modname, val = q.popleft()
+            if src == name and modname == "dg" and val == True:
+                if first is None:
+                    first = i
+                if prev is not None:
+                    cycle = i - prev
+                    return first, cycle
+                prev = i
+            if not modname in modules:
+                # ends here
+                continue
+            mod, dst = modules[modname]
+            result = mod.process(src, val)
+            for dst_name in dst:
+                if result is None:
+                    continue
+                q.append((modname, dst_name, result))
 
 # PART 2
 @measure_time
@@ -137,28 +164,13 @@ def solve2(data):
                 mod.add_input(srcname)
 
 
-    for i in range(100000000000000000000000000):
-        input()
-        q = deque([("button", "broadcaster", False)])
-        while q:
-            src, modname, val = q.popleft()
-            # input()
-            #if modname in ["lk", "zv", "sp", "xt"] and val == True:
-            # if modname == "dg" and val == True:
-            #     print()
-            #     print(f"{i=}, {src} -{'high' if val else 'low'}-> {modname}", end="")
-            if modname == "rx" and not val:
-                return i
-            if not modname in modules:
-                # ends here
-                continue
-            mod, dst = modules[modname]
-            result = mod.process(src, val)
-            for dst_name in dst:
-                if result is None:
-                    continue
-                q.append((modname, dst_name, result))
+    off1, cycle1 = get_first_low_and_cycle(modules, "xt")
+    off2, cycle2 = get_first_low_and_cycle(modules, "sp")
+    off3, cycle3 = get_first_low_and_cycle(modules, "zv")
+    off4, cycle4 = get_first_low_and_cycle(modules, "lk")
 
+    # don't understand why i can ignore the offsets ...
+    return math.lcm(cycle1, cycle2, cycle3, cycle4)
 
 
 if __name__ == "__main__":
